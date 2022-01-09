314c8ca4b5280838860c1b28b712e911fa79948a
stillcalm <2468336678@qq.com>
2022-01-09 19:11:12 +0800

    2020.1.9

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,37 @@
-// pages/cart/index.js
+/* 
+1 获取用户的收货地址
+  1 绑定点击事件
+  2 调用小程序内置api  获取用户的收货地址 chooseAddress
+2 页面加载完毕
+  0 onLoad onShow
+  1 获取本地储存中的地址数据
+  2 把数据 设置给data中的一个变量
+ */
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    address:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow(){
+    // 1 获取缓存中的收货地址信息
+    const address=wx.getStorageSync("address");
+    // 2 给data赋值
+    this.setData({
+      address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击 收货地址
+  async handleChooseAdress(){
+    // 2 获取收货地址
+    wx.chooseAddress({
+      success: (result) => {
+       console.log(result); 
+      }
+    });
+    // 4 调用获取收货地址的api
+    let address = await wx.chooseAddress();
+    address.all=address.provinceName+address.cityName+address.countyName+address.detailInfo;
+    // 5 存入到缓存中
+    wx.setStorageSync("address", address);
+      
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -22,7 +22,7 @@ Page({
   data: {
     goodsObj: {}
   },
-  GoodsInfo:{},
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
@@ -34,29 +34,56 @@ Page({
   // 获取商品详情数据
   async getGoodsDetail(goods_id) {
     const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
-    this.GoodsInfo=goodsObj;
+    this.GoodsInfo = goodsObj;
     this.setData({
-      goodsObj:{
-      goods_name:goodsObj.goods_name,
-      goods_price:goodsObj.goods_price,
-      // iPhone部分手机 不识别 webp图片格式
-      // 最好找到后台 让他进行修改
-      // 临时自己改 确保后台存在 1.webp => 1.jpg
-      goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
-      pics:goodsObj.pics 
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        // iPhone部分手机 不识别 webp图片格式
+        // 最好找到后台 让他进行修改
+        // 临时自己改 确保后台存在 1.webp => 1.jpg
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
       }
     })
   },
   // 点击轮播图放大预览
-  handlePreviewImage(e){
+  handlePreviewImage(e) {
     // 1 先构造要预览的图片数组
-    const urls=this.GoodsInfo.pics.map(v=>v.pics_mid);
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
     // 2 接受传递过来的图片url
-    const current=e.currentTarget.dataset.url;
+    const current = e.currentTarget.dataset.url;
     wx.previewImage({
       current,
       urls
     });
-      
+
+  },
+  // 点击加入购物车
+  handleCartAdd() {
+    // 1 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2 判断 商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      // 3 不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    } else {
+      // 4 已经存在的购物车数据执行 num++
+      cart[index].num++;
+    }
+    // 5 把购物车重新添加回缓存中
+    wx.setStorageSync("cart", cart);
+    // 6 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // true 防止用户手抖 疯狂点击按钮
+      mask: true,
+      duration:700
+    });
+
+
   }
 })
